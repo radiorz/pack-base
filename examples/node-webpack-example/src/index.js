a10b83cb67893cd7d596374c09527b74a91d6728
tikkhun <s565338107@163.com>
2024-05-10 09:25:56 +0800

    chore: 新建pack base。

--- examples/node-webpack-example/src/index.js
+++ examples/node-webpack-example/src/index.js
@@ -1,10 +1,9 @@
-import { a } from "./read.js";
 async function bootstrap() {
   while (true) {
     const timeout = 1000;
     await new Promise((resolve) => setTimeout(resolve, timeout));
-    console.log(`a`, a);
-    console.log(`123`,123)
+    import("a").then((a) => console.log(a));
+    console.log(`123`, 123);
   }
 }
 bootstrap();
